765073f24270e1aaaaf91d5ad4b55d6f194d725a
david darío villegas vásquez <davidvillegas1@hotmail.com>
2026-02-11 21:15:03 +0000

    Nueva linea

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Mi primera App en Flet sobre codespaces!!!"
     page.add(ft.Text("Hola a mundo Barquisimeto II!"))
+    page.add(ft.Text("Nueva línea de texto!"))
     
 ft.run(main)
