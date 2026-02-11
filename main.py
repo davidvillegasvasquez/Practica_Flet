693ef01d7fa43eb29da91d1b5c12bfaf8d4a797f
david darío villegas vásquez <davidvillegas1@hotmail.com>
2026-02-11 16:18:21 -0400

    Update greeting text and app execution method

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = "Mi App Flet en Codespaces"
-    page.add(ft.Text("Hola, mundo!"))
 
-ft.app(target=main, view=ft.AppView.WEB_BROWSER) # Ejecutar en navegador
+def main(page: ft.Page):
+    page.add(ft.Text("Hello, world!"))
+
+
+ft.run(main)
