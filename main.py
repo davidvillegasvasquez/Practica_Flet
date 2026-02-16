5bf04409343cb3d0616ca6d0efb5ca7a391f44ab
davidvillegasvásquez <davidvillegas1@hotmail.com>
2026-02-16 18:22:37 -0400

    260216: gui de la calculadora

--- main.py
+++ main.py
@@ -1,52 +1,83 @@
+from dataclasses import field
+
 import flet as f
 
 
 def main(pagina: f.Page):
     pagina.title = "Calc App"
-    result = f.Text(value="0")
+    result = f.Text(value="0", color=f.Colors.WHITE, size=20)
+
+    @f.control
+    class CalcButton(f.Button):
+        expand: int = field(default_factory=lambda: 1)
+
+    @f.control
+    class DigitButton(CalcButton):
+        bgcolor: f.Colors = f.Colors.WHITE_24
+        color: f.Colors = f.Colors.WHITE
+
+    @f.control
+    class ActionButton(CalcButton):
+        bgcolor: f.Colors = f.Colors.ORANGE
+        color: f.Colors = f.Colors.WHITE
+
+    @f.control
+    class ExtraActionButton(CalcButton):
+        bgcolor: f.Colors = f.Colors.BLUE_GREY_100
+        color: f.Colors = f.Colors.BLACK
 
     pagina.add(
-        f.Row(controls=[result]),
-        f.Row(
-            controls=[
-                f.Button("AC"),
-                f.Button("+/-"),
-                f.Button("%"),
-                f.Button("/"),
-            ]
-        ),
-        f.Row(
-            controls=[
-                f.Button("7"),
-                f.Button("8"),
-                f.Button("9"),
-                f.Button("*"),
-            ]
-        ),
-        f.Row(
-            controls=[
-                f.Button("4"),
-                f.Button("5"),
-                f.Button("6"),
-                f.Button("-"),
-            ]
-        ),
-        f.Row(
-            controls=[
-                f.Button("1"),
-                f.Button("2"),
-                f.Button("3"),
-                f.Button("+"),
-            ]
-        ),
-        f.Row(
-            controls=[
-                f.Button("0"),
-                f.Button("."),
-                f.Button("="),
-            ]
-        ),
-    )
+        f.Container(
+            width=350,
+            bgcolor=f.Colors.BLACK,
+            border_radius=f.BorderRadius.all(20),
+            padding=20,
+            content=f.Column(
+                controls=[
+                    f.Row(controls=[result], alignment=f.MainAxisAlignment.END),
+                    f.Row(
+                        controls=[
+                            ExtraActionButton(content="AC"),
+                            ExtraActionButton(content="+/-"),
+                            ExtraActionButton(content="%"),
+                            ActionButton(content="/"),
+                        ]
+                    ),
+                    f.Row(
+                        controls=[
+                            DigitButton(content="7"),
+                            DigitButton(content="8"),
+                            DigitButton(content="9"),
+                            ActionButton(content="*"),
+                        ]
+                    ),
+                    f.Row(
+                        controls=[
+                            DigitButton(content="4"),
+                            DigitButton(content="5"),
+                            DigitButton(content="6"),
+                            ActionButton(content="-"),
+                        ]
+                    ),
+                    f.Row(
+                        controls=[
+                            DigitButton(content="1"),
+                            DigitButton(content="2"),
+                            DigitButton(content="3"),
+                            ActionButton(content="+"),
+                        ]
+                    ),
+                    f.Row(
+                        controls=[
+                            DigitButton(content="0", expand=2),
+                            DigitButton(content="."),
+                            ActionButton(content="="),
+                        ],
+                    ),
+                ] #Cierre del controls de la primera columna del container
+            ), #Cierre del content del container
+        ) #Cierre del container
+    ) #Cierre del add
 
 
 if __name__ == "__main__":
